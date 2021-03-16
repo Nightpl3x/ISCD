32bbd3a95e1b560757015c348c43b0ba11b61278
chr-ohl <chr.ohl@wunder-koestner.de>
2021-03-16 15:54:19 +0100

    reallocated image variable in v.py

--- v.py
+++ v.py
@@ -3,10 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 '''
+image selection
+'''
+image = cv2.imread('images\S2_Kovacs_10000Z.jpeg') # :Workplace relative path
+
+'''
 image processing variables
 '''
 # read images via the image path (Important: Only use the pah of your current device and comment the other out!)
-image = cv2.imread('images/0H_10000Z.jpeg') # :Workplace relative path
 image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB) #convert to rgb just to be safe later on
 image_copy = np.copy(image) #copying the image so we dont alter the original
 image_copy_HSV = cv2.cvtColor(image_copy, cv2.COLOR_RGB2HSV) #create a copy to test hsv
