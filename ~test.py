77b9cc0beb204b559fd16fa153aeb49f03ade865
chr-ohl <chr.ohl@wunder-koestner.de>
2021-03-23 11:21:19 +0100

    fix

--- ~test.py
+++ ~test.py
@@ -3,18 +3,3 @@ import numpy as np
 import matplotlib.pyplot as plt
 import variables as v
 
-# https://learnopencv.com/hough-transform-with-opencv-c-python/amp/
-
-image_circles = cv2.HoughCircles(v.image_edges, cv2.HOUGH_GRADIENT, 1, 20, param1=20, param2=10, minRadius=25, maxRadius=35) # Apply Hough transform on the image
-
-if image_circles is not None: # Draw detected circles
-    image_circles = np.uint16(np.around(image_circles))
-    for i in image_circles[0, :]:
-        
-        cv2.circle(v.image_copy, (i[0], i[1]), i[2], (0, 0, 255), 1) # Draw outer circle
-       
-        cv2.circle(v.image_copy, (i[0], i[1]), 2, (255, 0, 0), 1) # Draw inner circle
-
-plt.imshow(v.image_copy)
-plt.show()
-
