a43966fc3a4935a8159d227a195fec8d37a0155e
chr-ohl <chr.ohl@wunder-koestner.de>
2021-03-25 10:36:02 +0100

    had to rename the varables import 'v' in visualize.py to 'var' as the v channel of the hsv color space doesnt like it that they both share the same letter

--- visualize.py
+++ visualize.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import variables as v
+import variables as var
 
 '''
 Additional imports:
@@ -18,7 +18,7 @@ def rgb_visualize():
     '''
     Setup:
     '''
-    img = v.image_copy
+    img = var.image_copy
     r, g, b = cv2.split(img)
     fig = plt.figure()
     axis = fig.add_subplot(1, 1, 1, projection="3d")
@@ -37,9 +37,32 @@ def rgb_visualize():
     axis.set_zlabel("Blue")
     plt.show()
 
+def hsv_visualize():
 
+    '''
+    Setup:
+    '''
+    img = var.image_HSV
+    h, s, v = cv2.split(img)
+    fig = plt.figure()
+    axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+    '''
+    Create Graphic:
+    '''
+    pixel_colors = img.reshape((np.shape(img)[0]*np.shape(img)[1], 3))
+    norm = colors.Normalize(vmin=-1.,vmax=1.)
+    norm.autoscale(pixel_colors)
+    pixel_colors = norm(pixel_colors).tolist()
+
+    axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
+    axis.set_xlabel("Hue")
+    axis.set_ylabel("Saturation")
+    axis.set_zlabel("Value")
+    plt.show()
 
 '''
 Call Functions:
 '''
 #rgb_visualize()
+hsv_visualize()
