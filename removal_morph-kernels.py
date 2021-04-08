b99aa0e32fcdb5531603a35b8dc4c5921dd43977
chr-ohl <chr.ohl@wunder-koestner.de>
2021-04-08 11:22:53 +0200

    various changes to removal_morph_kernels.py

--- removal_morph-kernels.py
+++ removal_morph-kernels.py
@@ -3,42 +3,61 @@ import numpy as np
 import variables as v
 
 img = v.image_copy
+'''
+Kernels:
+'''
+kernel = np.ones((5,5), np.uint8) # 5x5 Einheitsmatrix
+kernel2 = np.ones((7,7), np.uint8) # 7x7 Einheitsmatrix
+kernel3 = np.array(([0, 1, 0],
+					[1, 1, 1],
+					[0, 1, 0]),dtype='uint8') # Wikipedia Test Matrix
 
-kernel = np.ones((5,5), np.uint8)
-kernel2 = np.ones((7,7), np.uint8)
-kernel3 = np.array((
-	[0, 1, 0],
-	[1, 1, 1],
-	[0, 1, 0]),dtype='uint8')
+'''
+Basic modification:
+'''
+img_open = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+img_close = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
-img_opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-img_closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+img_openXclose = cv2.morphologyEx(img_open, cv2.MORPH_CLOSE, kernel)
 
-img_erosion = cv2.erode(img, kernel3, iterations=1)
-img_dilation = cv2.dilate(img_erosion, kernel3, iterations=1)
-img_opening3 = cv2.morphologyEx(img_dilation, cv2.MORPH_OPEN, kernel2)
-img_closing2 = cv2.morphologyEx(img_dilation, cv2.MORPH_CLOSE, kernel)
-img_opening2 = cv2.morphologyEx(img_closing2, cv2.MORPH_OPEN, kernel3)
+'''
+Advanced modification:
+'''
+img_erode = cv2.erode(img, kernel3, iterations=1)
+img_dilate = cv2.dilate(img_erode, kernel3, iterations=1)
 
+img_close2 = cv2.morphologyEx(img_dilate, cv2.MORPH_CLOSE, kernel2)
 
-opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-close = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+img_open2 = cv2.morphologyEx(img_dilate, cv2.MORPH_OPEN, kernel2)
+img_open3 = cv2.morphologyEx(img_close2, cv2.MORPH_OPEN, kernel3)
 
+'''
+Show:
+'''
+# Basics:
 cv2.imshow('Input', img) 
-cv2.imshow('Opening', img_opening)
-cv2.imshow('Closing', img_closing)
-cv2.imshow('Erosion Plus Dilation', img_dilation)
-cv2.imshow('Erode + Dilate + Close',img_closing2)
-cv2.imshow('Processed',img_closing2) 
-cv2.imshow('Improved', close) 
+cv2.imshow('Close', img_close)
+cv2.imshow('Open', img_open)
+
+cv2.imshow('Open + Close', img_openXclose)
+
+# Advanced:
+cv2.imshow('Erode + Dilate', img_dilate)
+
+cv2.imshow('Erode + Dilate + Close',img_close2)
+
+cv2.imshow('Erode + Dilate + Open',img_open2) 
+
+cv2.imshow('Erode + Dilate + Close + Open',img_open3) 
+ 
 
 '''
 cv2.imwrite('Input.png', img) 
-cv2.imwrite('Opening.png', img_opening)
-cv2.imwrite('Closing.png', img_closing)
-cv2.imwrite('ErosionPlusDilation.png', img_dilation)
-cv2.imwrite('ErodeDilateClose.png',img_closing2)
-cv2.imwrite('Processed.png',img_closing2)
+cv2.imwrite('Opening.png', img_open)
+cv2.imwrite('Closing.png', img_close)
+cv2.imwrite('ErosionPlusDilation.png', img_dilate)
+cv2.imwrite('ErodeDilateClose.png',img_close2)
+cv2.imwrite('Processed.png',img_close2)
 cv2.imwrite('Final-ComparisionImage.png', img2) 
 cv2.imwrite('Improved.png', close) 
 '''
