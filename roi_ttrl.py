85a799162e3b5b641d0e647e22f28f61edd78bac
chr-ohl <chr.ohl@wunder-koestner.de>
2021-04-09 13:51:47 +0200

    update

--- roi_ttrl.py
+++ roi_ttrl.py
@@ -66,7 +66,8 @@ def get_contours(img):
 '''
 File specific variables:
 '''
-path_shapes = "images_testing/shapes.jpeg"
+#path_shapes = "images_testing/shapes.png"
+path_shapes = "images/28H_10000Z.jpeg"
 
 img = cv2.imread(path_shapes)
 #img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) # convert to rgb
