c27b5755a42a0e98867d90a4a242d3f839ca546b
chr-ohl <chr.ohl@wunder-koestner.de>
2021-04-12 08:48:14 +0200

    moved stackImages to variables.py for general use

--- roi_ttrl.py
+++ roi_ttrl.py
@@ -3,49 +3,24 @@ Imports:
 '''
 import cv2
 import numpy as np
+import variables as v
 
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+'''
+Function:
+'''
 
 def get_contours(img):
 
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours: # cnt represents contour values
         area = cv2.contourArea(cnt)
-        # print(area) # maybe use values to cut to edges?
+        #print(area) # maybe use values to cut to edges?
         
         if 60 > area > 15: # adjust for needed object size
             cv2.drawContours(img_contour, cnt, -1, (255,0,0), 3)
             peri = cv2.arcLength(cnt, True) # get contour parameter
-            approx = cv2.approxPolyDP(cnt, 0.01*peri, True) #adjust here for image
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt, 0.01*peri, True) # adjust here for image
             objCor = len(approx)
             x, y, w, h = cv2.boundingRect(approx)
 
@@ -66,8 +41,8 @@ def get_contours(img):
 '''
 File specific variables:
 '''
-path_shapes = "images_testing/shapes.jpeg"
-#path_shapes = "images/S2_Kovacs_10000Z.jpeg"
+#path_shapes = "images_testing/shapes.jpeg"
+path_shapes = "images/S2_Kovacs_10000Z.jpeg"
 
 img = cv2.imread(path_shapes)
 #img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) # convert to rgb
@@ -79,7 +54,8 @@ img_canny = cv2.Canny(img_blur,50,50)
 get_contours(img_canny)
 
 img_blank = np.zeros_like(img)
-img_stacked = stackImages(0.8, ([img, img_gray, img_blur],
+
+img_stacked = v.stackImages(0.8, ([img, img_gray, img_blur],
                                 [img_canny, img_contour, img_blank]))
 
 cv2.imshow("Stacked", img_stacked)
