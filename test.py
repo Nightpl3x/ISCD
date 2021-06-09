7850752cefecd34ec2848e86a15d21e604ba9ae9
chr-ohl <chr.ohl@wunder-koestner.de>
2021-06-09 15:01:26 +0200

    changes

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 # ==========================================================================================================================================================#
-#                                                          directoryHandling.py 
-#               file for moving images in the image_1_camera directory into the corresponding directory below the image_3_storage directory
+#                                                                       test.py 
 # ==========================================================================================================================================================#
 
 # =============================================================================
@@ -23,12 +22,6 @@ from utils import VariableConfig as vxct
 from extras import test_functions
 
 # ==========================================================================
-#   Setup
+#   Test
 # ==========================================================================
 
-e = xct.path_folder_cam+"/*.jpeg" # check ROI result folder for images
-
-IMAGE_DIRECTORY = glob.glob(e) # create list based on image names --> strings
-IMAGE_DIRECTORY.sort()         # sort list
-images = [cv2.imread(img) for img in IMAGE_DIRECTORY] # create additional list for storing images --> ndarrays
-images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in images] # convert from bgr back to rgb
